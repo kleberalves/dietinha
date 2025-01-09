385f622257f97fe6639d40dba8e72259afd99f00
Kleber Alves <kleber@kleberalves.com.br>
2025-01-09 15:34:40 -0300

    design e calculos

--- script.calculadora.alimentos.js
+++ script.calculadora.alimentos.js
@@ -1,8 +1,34 @@
 //Calcular calorias dos alimentos
 var listaAlimentos = new Array();
-var listaHistorico = new Array();
+var listaCalculos = new Array();
 var outputListaAlimentos = document.querySelector('#outputListaAlimentos');
 var outputHistoricoCalculos = document.querySelector('#outputHistoricoCalculos');
+
+function txtPequisaAlterado(alimentoDigitadoValue) {
+
+    if (alimentoDigitadoValue === "") {
+        outputListaAlimentos.innerHTML = "";
+    } else {
+        var msg = "";
+
+        //Lista os produtos na tela com o botão calcular em cada um dos produtos
+        for (var i = 0; i < listaAlimentos.length; i++) {
+
+            if (listaAlimentos[i].nome.toLowerCase().indexOf(alimentoDigitadoValue.toLowerCase()) > -1) {
+                msg += "<div class='rowList'>";
+                msg += "<div class='title'>" + listaAlimentos[i].nome + "<span>" + listaAlimentos[i].calorias + "cal por " + listaAlimentos[i].peso + listaAlimentos[i].unidade + "</span></div> ";
+                msg += "<div class='actions'>";
+                msg += "<input type='number' id='itemResultadoPeso" + i + "' style='width: 70px;' placeholder='peso' oninput='calcularCaloriaProduto(this.value, " + i + ", " + listaAlimentos[i].id + ")' />";
+                msg += "<div class='action'><b>Calorias</b><div id='itemResultadoCalorias" + i + "'>-</div></div>";
+                msg += "<div class='action'><b>Proteínas</b><div id='itemResultadoProteinas" + i + "'>-</div></div>";
+                msg += "<button onclick=\"adicionarCalculo(" + i + ", " + listaAlimentos[i].id + ")\"> Adicionar </button></div></div>";
+            }
+        }
+
+        outputListaAlimentos.innerHTML = msg;
+    }
+}
+
 
 function buscarProdutoPorId(idProduto) {
 
@@ -18,40 +44,71 @@ function buscarProdutoPorId(idProduto) {
     return null;
 }
 
-function calcularCaloriaProduto(idProduto) {
+function calcularCaloriaProduto(peso, idxResultado, idProduto) {
+
+    var itemResultadoCalorias = document.getElementById("itemResultadoCalorias" + idxResultado);
+    var itemResultadoProteinas = document.getElementById("itemResultadoProteinas" + idxResultado);
 
     var produto = buscarProdutoPorId(idProduto);
 
-    var peso = prompt('Insira o peso:');
-    peso = parseFloat(peso);
+    if (peso !== "" && peso !== null && !isNaN(peso)) {
+        peso = parseFloat(peso);
 
-    var proteinas = Math.round((peso * produto.proteina) / produto.peso);
-    var calorias = Math.round((peso * produto.calorias) / produto.peso);
+        var proteinas = Math.round((peso * produto.proteina) / produto.peso);
+        var calorias = Math.round((peso * produto.calorias) / produto.peso);
 
-    var msgResultado = produto.nome + ": " + calorias + " calorias e ";
-    msgResultado += proteinas + " proteínas por " + peso + produto.unidade;
-    msgResultado += " <br/><button class='btnCafeManha' onclick='addDieta(1, " + idProduto + "," + proteinas + "," + calorias + "," + peso + ")'> Adicionar café da manhã </button>"
-    msgResultado += " <button class='btnAlmoco' onclick='addDieta(2, " + idProduto + "," + proteinas + "," + calorias + "," + peso + ")'> Adicionar almoço </button>"
-    msgResultado += " <button class='btnCafeLancheTarde' onclick='addDieta(3, " + idProduto + "," + proteinas + "," + calorias + "," + peso + ")'> Adicionar café/lanche da tarde </button>"
-    msgResultado += " <button class='btnJantar' onclick='addDieta(4, " + idProduto + "," + proteinas + "," + calorias + "," + peso + ")'> Adicionar jantar </button>"
-    msgResultado += "<hr/>";
-
-    listaHistorico.push(msgResultado);
-
-    var msgHistorico = "<h4>Histórico</h4>";
-
-    for (var i = 0; i < listaHistorico.length; i++) {
-        msgHistorico += listaHistorico[i];
+        itemResultadoCalorias.innerHTML = calorias;
+        itemResultadoProteinas.innerHTML = proteinas;
+    } else {
+        itemResultadoCalorias.innerHTML = Math.round((peso * produto.calorias) / produto.peso);
+        itemResultadoProteinas.innerHTML = Math.round((peso * produto.proteina) / produto.peso);
     }
+}
+
+function adicionarCalculo(idxResultado, idProduto) {
+
+    var produto = buscarProdutoPorId(idProduto);
+
+    var itemResultadoCaloriasValue = parseFloat(document.getElementById("itemResultadoCalorias" + idxResultado).innerText);
+    var itemResultadoProteinasValue = parseFloat(document.getElementById("itemResultadoProteinas" + idxResultado).innerText);
+    var itemResultadoPesoValue = parseFloat(document.getElementById("itemResultadoPeso" + idxResultado).value);
+
+    listaCalculos.push({
+        "nome": produto.nome,
+        "calorias": itemResultadoCaloriasValue,
+        "proteinas": itemResultadoProteinasValue,
+        "peso": itemResultadoPesoValue,
+        "unidade": produto.unidade
+    });
+
+
+    listaHistoricoCalculos();
+
+
+    document.getElementById("txtPesquisa").value = "";
+    outputListaAlimentos.innerHTML = "";
+}
+
+function listaHistoricoCalculos() {
+    var msgHistorico = "<b>Cálculos</b>";
+    var totalCalorias = 0;
+    var totalProteinas = 0;
+
+    for (var i = 0; i < listaCalculos.length; i++) {
+        var itemCalculo = listaCalculos[i];
+
+        msgHistorico += "<hr/><b>" + itemCalculo.nome + "</b> " + itemCalculo.calorias + " calorias e ";
+        msgHistorico += itemCalculo.proteinas + " proteínas em " + itemCalculo.peso + itemCalculo.unidade;
+
+        totalCalorias += itemCalculo.calorias;
+        totalProteinas += itemCalculo.proteinas;
+    }
+
+    msgHistorico += "<br/><br/><b> Total Calorias</b> " + totalCalorias;
+    msgHistorico += "<br/><b> Total Proteínas</b> " + totalProteinas;
 
     outputHistoricoCalculos.innerHTML = msgHistorico;
 }
-
-function addDieta(numOperacao, idProduto, qtdProteinas, qtdCalorias, peso) {
-    alert(numOperacao + " " + idProduto);
-}
-
-
 
 listaAlimentos.push({
     "id": 1,
@@ -85,7 +142,8 @@ listaAlimentos.push({
     "nome": "Pão Francês",
     "calorias": 280,
     "peso": 100,
-    "unidade": "g"
+    "unidade": "g",
+    "proteina": 0
 });
 
 listaAlimentos.push({
@@ -93,7 +151,8 @@ listaAlimentos.push({
     "nome": "Batata Inglesa",
     "calorias": 52,
     "peso": 100,
-    "unidade": "g"
+    "unidade": "g",
+    "proteina": 0
 });
 
 listaAlimentos.push({
@@ -158,24 +217,3 @@ listaAlimentos.push({
     "unidade": "g",
     "proteina": 0
 });
-
-
-function txtPequisaAlterado() {
-    var alimentoDigitado = document.getElementById("txtPesquisa").value;
-
-    if (alimentoDigitado === "") {
-        outputListaAlimentos.innerHTML = "";
-    } else {
-        var msg = "";
-
-        //Lista os produtos na tela com o botão calcular em cada um dos produtos
-        for (var i = 0; i < listaAlimentos.length; i++) {
-
-            if (listaAlimentos[i].nome.toLowerCase().indexOf(alimentoDigitado) > -1) {
-                msg += "<h4 style='margin-bottom:0px; margin-top:10px'>" + listaAlimentos[i].nome + "</h4> " + listaAlimentos[i].calorias + "cal por " + listaAlimentos[i].peso + listaAlimentos[i].unidade + " <button onclick=\"calcularCaloriaProduto(" + listaAlimentos[i].id + ")\"> Calcular </button>";
-            }
-        }
-
-        outputListaAlimentos.innerHTML = msg;
-    }
-}
--- script.consumo.diario.js
+++ script.consumo.diario.js
@@ -1,79 +1,83 @@
- //Calcular consumo diário calorias
- var listaCaloria = new Array();
+//Calcular consumo diário calorias
+var listaCaloria = new Array();
 
- function calcularConsumoDiario() {
+function calcularConsumoDiario() {
 
-     var numPeso = document.getElementById("inputPeso").value;
-     numPeso = parseFloat(numPeso);
+    var numPeso = document.getElementById("inputPeso").value;
+    numPeso = parseFloat(numPeso);
 
-     var opcaoAtividadeFisica = document.querySelector('input[name=inputAtividadeFisica]:checked').value;
-     opcaoAtividadeFisica = parseInt(opcaoAtividadeFisica);
+    var opcaoAtividadeFisica = document.querySelector('input[name=inputAtividadeFisica]:checked').value;
+    opcaoAtividadeFisica = parseInt(opcaoAtividadeFisica);
 
-     var lista = document.querySelector('#lista');
+    var lista = document.querySelector('#lista');
 
-     var genero = document.querySelector('input[name=inputGenero]:checked').value;
+    var genero = document.querySelector('input[name=inputGenero]:checked').value;
 
-     var altura = document.getElementById("inputAltura").value;
-     altura = parseInt(altura);
+    var altura = document.getElementById("inputAltura").value;
+    altura = parseInt(altura);
 
-     var idade = document.getElementById("inputIdade").value;
-     idade = parseInt(idade);
+    var idade = document.getElementById("inputIdade").value;
+    idade = parseInt(idade);
 
-     if (!isNaN(numPeso) || !isNaN(opcaoAtividadeFisica)) {
+    if (!isNaN(numPeso) || !isNaN(opcaoAtividadeFisica)) {
 
-         var numTMB = 0;
+        var numTMB = 0;
 
-         //masculino
-         if (genero === "1") {
-             numTMB = 88.362 + (13.397 * numPeso) + (4.799 * altura) - (5.677 * idade);
-         }
-         // feminino  
-         if (genero === "2") {
-             numTMB = 447.593 + (9.247 * numPeso) + (3.098 * altura) - (4.330 * idade);
-         }
+        //masculino
+        if (genero === "1") {
+            numTMB = 88.362 + (13.397 * numPeso) + (4.799 * altura) - (5.677 * idade);
+        }
+        // feminino  
+        if (genero === "2") {
+            numTMB = 447.593 + (9.247 * numPeso) + (3.098 * altura) - (4.330 * idade);
+        }
 
-         //var numTMB = numPeso * 22;
+        //var numTMB = numPeso * 22;
 
-         if (opcaoAtividadeFisica >= 1 && opcaoAtividadeFisica <= 3) {
+        if (opcaoAtividadeFisica >= 1 && opcaoAtividadeFisica <= 3) {
 
-             var resultadoCaloriasPorDia = 0;
+            var resultadoCaloriasPorDia = 0;
 
-             if (opcaoAtividadeFisica === 1) {
-                 resultadoCaloriasPorDia = numTMB * 1.4;
-             }
+            if (opcaoAtividadeFisica === 1) {
+                resultadoCaloriasPorDia = numTMB * 1.4;
+            }
 
-             if (opcaoAtividadeFisica === 2) {
-                 resultadoCaloriasPorDia = numTMB * 1.6;
-             }
+            if (opcaoAtividadeFisica === 2) {
+                resultadoCaloriasPorDia = numTMB * 1.6;
+            }
 
-             if (opcaoAtividadeFisica === 3) {
-                 resultadoCaloriasPorDia = numTMB * 1.8;
-             }
+            if (opcaoAtividadeFisica === 3) {
+                resultadoCaloriasPorDia = numTMB * 1.8;
+            }
 
-             resultadoCaloriasPorDia = Math.round(resultadoCaloriasPorDia);
+            resultadoCaloriasPorDia = Math.round(resultadoCaloriasPorDia);
 
-             var resultadoParaEmagrecer = Math.round(resultadoCaloriasPorDia - (resultadoCaloriasPorDia * 0.2));
+            var resultadoParaEmagrecer = Math.round(resultadoCaloriasPorDia - (resultadoCaloriasPorDia * 0.2));
 
-             var resultadoParaGanharMassa = Math.round(resultadoCaloriasPorDia + (resultadoCaloriasPorDia * 0.1));
+            var resultadoParaGanharMassa = Math.round(resultadoCaloriasPorDia + (resultadoCaloriasPorDia * 0.1));
 
-             var resultado = "<hr/>Calorias para manter o seu peso: <b>" + resultadoCaloriasPorDia + "</b>. <br/>Para emagrecer: <b>" + resultadoParaEmagrecer + "</b>. <br/>Para ganhar massa: <b>" + resultadoParaGanharMassa + "</b> ";
-             
-             listaCaloria.push(resultado);
-         
-             var msg = "";
+            var resultadoProteinas = Math.round(numPeso * 1.6);
 
-             for (var i = 0; i < listaCaloria.length; i++) {
-                 msg += ' <br> ' + listaCaloria[i];
-             }
+            var resultado = "<hr/>Calorias para manter o seu peso: <b>" + resultadoCaloriasPorDia + "</b>. <br/>Para emagrecer: <b>" + resultadoParaEmagrecer + "</b>. ";
+            resultado += "<br/>Para ganhar massa: <b>" + resultadoParaGanharMassa + "</b> ";
+            resultado += "<br/>Proteínas: <b>" + resultadoProteinas + "</b> ";
 
-             lista.innerHTML = msg;
+            listaCaloria.push(resultado);
 
-         } else {
-             output.innerHTML = "Digite de 1 a 3";
-         }
+            var msg = "";
 
-     } else {
-         output.innerHTML = "Digite opções numéricas.";
-     }
+            for (var i = 0; i < listaCaloria.length; i++) {
+                msg += ' <br> ' + listaCaloria[i];
+            }
 
- }
+            lista.innerHTML = msg;
+
+        } else {
+            output.innerHTML = "Digite de 1 a 3";
+        }
+
+    } else {
+        output.innerHTML = "Digite opções numéricas.";
+    }
+
+}
